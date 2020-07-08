4083bc22db116b2f6327c4babcceb6f291af5d94
Subramanya Adiga <Suramanay.adiga4@gmail.com>
2020-07-08 19:08:57 +0530

    addded basic command line handling.Implemented few commands

--- src/wall_app.cpp
+++ src/wall_app.cpp
@@ -1,24 +1,23 @@
 #include "wall_app.h"
 
 wallchanger::application::application(std::span<char *> args) {
-  ranges::for_each(m_desc_name, [&](const std::string &name) {
-    m_desc_vec.emplace_back(po::options_description(name));
+  ranges::for_each(m_desc_name, [&](auto &data) {
+    m_cmd_vec.emplace_back(0, data, po::options_description(data));
   });
-  m_add_options();
-  po::store(po::parse_command_line(args.size(), args.data(),
-                                   m_desc_vec[m_desc_pos::general]),
-            m_option_map);
+  auto ret = ranges::find(m_cmd_vec, m_desc_name[0], &commandline::cmd_name);
+  ranges::for_each(m_general_options, [&](auto &data) {
+    ret->cmd.add_options()(data.first.c_str(), data.second.c_str());
+  });
+
+  po::store(
+      po::parse_command_line(args.size(), args.data(), m_cmd_vec.front().cmd),
+      m_option_map);
   po::notify(m_option_map);
 }
 
 int wallchanger::application::run() {
-  if (m_option_map.count("help")) {
-    std::cout << m_desc_vec[0] << "\n";
-    return 1;
+  if (m_option_map.count("help") != 0U) {
+    std::cout << m_cmd_vec[0].cmd << "\n";
   }
   return 0;
-}
-
-void wallchanger::application::m_add_options() {
-  m_desc_vec[m_desc_pos::general].add_options()("help", "produce help message");
 }
--- src/wall_app.h
+++ src/wall_app.h
@@ -1,16 +1,32 @@
 #pragma once
 #include "../pch.h"
+#include "range/v3/algorithm/for_each.hpp"
 #include <boost/program_options.hpp>
 #include <span>
 
 namespace wallchanger {
 namespace po = boost::program_options;
 
+struct commandline {
+  int cmd_id;
+  std::string cmd_name;
+  po::options_description cmd;
+};
+
 class application {
   enum m_desc_pos { general, allowed, config, hidden };
   std::array<std::string, sizeof(m_desc_pos)> m_desc_name = {
-      "General Options", "Allowed Option", "Configuration Option",
-      "Hidden Option"};
+      "General Options", "Commands", "Configuration Option", "Hidden Option"};
+  std::vector<std::pair<std::string, std::string>> m_general_options = {
+      {"next,n", "next wallpaper"},
+      {"previous,p", "previous wallpaper"},
+      {"history,h", "print wallpaper history"},
+      {"get-current", "print current wallpaper name and location"},
+      {"set-wallpaper",
+       "set file as wallpaper or file at location as wallpaper"},
+      {"move-to-favoriate", "move current wallpaper to favoriate"},
+      {"version", "Program version"},
+      {"help,h", "Print Help Message"}};
 
 public:
   explicit application(std::span<char *> args);
@@ -18,7 +34,7 @@ public:
 
 private:
   po::variables_map m_option_map;
-  std::vector<po::options_description> m_desc_vec;
+  std::vector<commandline> m_cmd_vec;
   void m_add_options();
 };
 } // namespace wallchanger
