8c481d3cb2748b72071e834a1fb612fd608d3a86
Subramanya <subramanya.adiga4@protonmail.com>
2023-02-02 16:23:51 +0530

    Moved Or Deleted  Files.

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "src/wall_app.h"
-#include <iostream>
-
-int main(int argc, char *argv[]) {
-  wallchanger::application app({argv, static_cast<unsigned long>(argc)});
-  return app.run();
-}
