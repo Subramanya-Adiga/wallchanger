cc91e2bb15463741d82f8d351bd15786b26d266a
Subramanya <subramanya.adiga4@protonmail.com>
2023-02-04 22:26:28 +0530

    Updated Background Changer On Windows, For Better Error Handling.

--- src/platform/platform_win32.cpp
+++ src/platform/platform_win32.cpp
@@ -1,35 +1,49 @@
 #include "platform_win32.h"
+#include "windows_helper.h"
 
 wallchanger::platform::win32::background::background() {
-  if (HRESULT init = CoInitialize(NULL); init == S_OK) {
-    if (HRESULT instance_create =
-            CoCreateInstance(__uuidof(DesktopWallpaper), NULL, CLSCTX_ALL,
-                             IID_PPV_ARGS(&m_wallpaper));
-        instance_create == S_OK) {
-      m_valid_instance = true;
-    }
-    // log
-    m_initialized = true;
+  HRESULT res = CoInitialize(nullptr);
+  if (FAILED(res)) {
+    m_display_err("CoInitialize", res);
+    m_initialized = false;
   }
-  // log
+  m_initialized = true;
+
+  res = CoCreateInstance(__uuidof(DesktopWallpaper), nullptr, CLSCTX_ALL,
+                         IID_PPV_ARGS(&m_wallpaper));
+  m_valid_instance = true;
+  if (FAILED(res)) {
+    m_display_err("CoCreateInstance", res);
+    m_valid_instance = false;
+  }
 }
 
 wallchanger::platform::win32::background::~background() {
-  if (m_initialized)
+  if (m_initialized) {
+    m_wallpaper->Release();
     CoUninitialize();
+  }
 }
 
 bool wallchanger::platform::win32::background::set_wallpaper(
     std::wstring_view filename, int position) const {
   HRESULT result = S_FALSE;
   if (m_valid_instance) {
+
     result = m_wallpaper->SetWallpaper(NULL, filename.data());
-    if (!(position > DWPOS_CENTER) || (position > DWPOS_SPAN)) {
-      m_wallpaper->SetPosition(
-          static_cast<DESKTOP_WALLPAPER_POSITION>(position));
-    } else {
-      m_wallpaper->SetPosition(
-          static_cast<DESKTOP_WALLPAPER_POSITION>(position));
+    if (FAILED(result)) {
+      m_display_err("IDesktopWallpaper->SetWallpaper", result);
+      return false;
+    }
+
+    int pos =
+        ((position >= DWPOS_CENTER) && (position <= DWPOS_SPAN)) ? position : 0;
+
+    result =
+        m_wallpaper->SetPosition(static_cast<DESKTOP_WALLPAPER_POSITION>(pos));
+    if (FAILED(result)) {
+      m_display_err("IDestopWallpaper->SetPosition", result);
+      return false;
     }
   }
   return (result == S_OK);
@@ -39,6 +53,17 @@ bool wallchanger::platform::win32::background::set_color(DWORD color) const {
   HRESULT result = S_FALSE;
   if (m_valid_instance) {
     result = m_wallpaper->SetBackgroundColor(color);
+    if (FAILED(result)) {
+      m_display_err("IDestopWallpaper->SetBackgroundColor", result);
+      return false;
+    }
   }
   return (result == S_OK);
+}
+
+void wallchanger::platform::win32::background::m_display_err(
+    std::string_view name, HRESULT res) {
+  auto err_text =
+      ::win32::to_utf16(::win32::error_handler_win32::fmt_msg(name, res));
+  ::MessageBoxW(0, err_text.c_str(), L"Error", MB_ICONERROR | MB_OK);
 }
--- src/platform/platform_win32.h
+++ src/platform/platform_win32.h
@@ -1,42 +1,29 @@
 #pragma once
-#include "../../pch.h"
 #include <Shobjidl.h>
 namespace wallchanger::platform::win32 {
 
-struct error_handler_win32 {
-  error_handler_win32() = default;
-  error_handler_win32(error_handler_win32 &&) = delete;
-  error_handler_win32(const error_handler_win32 &) = delete;
-  error_handler_win32 &operator=(error_handler_win32 &&) = delete;
-  error_handler_win32 &operator=(const error_handler_win32 &) = delete;
-  ~error_handler_win32() {
-    if (m_msg_buf != NULL)
-      LocalFree(m_msg_buf);
-  }
-  std::string fmt_msg(std::string_view func_name) {
-    auto id = GetLastError();
-    FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-                      FORMAT_MESSAGE_IGNORE_INSERTS,
-                  nullptr, id, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-                  reinterpret_cast<LPTSTR>(&m_msg_buf), 0, nullptr);
-    return fmt::format("{} failed with error {}: {}", func_name, id,
-                       static_cast<LPTSTR>(m_msg_buf));
-  }
-
-private:
-  LPVOID m_msg_buf = nullptr;
-};
-
 class background {
   IDesktopWallpaper *m_wallpaper = nullptr;
-  bool m_initialized = false;
-  bool m_valid_instance = false;
+  bool m_initialized;
+  bool m_valid_instance;
 
 public:
   background();
   ~background();
-  bool is_active() const { return (m_initialized && m_valid_instance); }
-  bool set_wallpaper(std::wstring_view filename, int position) const;
-  bool set_color(DWORD color) const;
+  background(const background &) = delete;
+  background(background &&) = delete;
+  background &operator=(const background &) = delete;
+  background &operator=(background &&) = delete;
+
+  [[nodiscard]] bool is_active() const {
+    return (m_initialized && m_valid_instance);
+  }
+  [[nodiscard]] bool set_wallpaper(std::wstring_view filename,
+                                   int position) const;
+  [[nodiscard]] bool set_color(DWORD color) const;
+
+private:
+  static void m_display_err(std::string_view name, HRESULT res);
 };
+
 } // namespace wallchanger::platform::win32
