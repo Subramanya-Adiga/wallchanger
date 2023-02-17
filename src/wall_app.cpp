9d2ec25b8c3f63d3d81e81d499f56eed1ea42110
Subramanya <subramanya.adiga4@protonmail.com>
2023-02-17 11:50:22 +0530

    Added Proper Handling Of Subcommands.

--- src/wall_app.cpp
+++ src/wall_app.cpp
@@ -3,59 +3,88 @@
 
 wallchanger::application::application(std::span<char *> args) {
   LOGGER_CREATE("changer");
+
+  po::positional_options_description pos;
+  pos.add("command", 1).add("subargs", -1);
+
+  // Initialize All Commandgroups
   ranges::for_each(m_group_name, [&](auto &data) {
     m_group_vec.emplace_back(data, po::options_description(data));
   });
-  for (auto &&[f, s] : m_cmds) {
-    auto group = ranges::find(m_group_vec, f, &commandgroup::first);
-    ranges::for_each(s, [&](auto &data) {
-      if (std::get<2>(data) != nullptr) {
-        group->second.add_options()(std::get<0>(data).data(), std::get<2>(data),
-                                    std::get<1>(data).data());
+
+  // Loop To Add Appropriate Commandline Arguments To commandgroup
+  for (auto &&[group_name, commandline] : m_cmds) {
+    auto group = ranges::find(m_group_vec, group_name, &commandgroup::first);
+    ranges::for_each(commandline, [&](auto &data) {
+      if (std::get<value>(data) != nullptr) {
+        group->second.add_options()(std::get<cmd>(data).data(),
+                                    std::get<value>(data),
+                                    std::get<desc>(data).data());
       } else {
-        group->second.add_options()(std::get<0>(data).data(),
-                                    std::get<1>(data).data());
+        group->second.add_options()(std::get<cmd>(data).data(),
+                                    std::get<desc>(data).data());
       }
     });
   }
-  po::store(po::parse_command_line(static_cast<int>(args.size()), args.data(),
-                                   m_group_vec.front().second),
-            m_option_map);
-  po::notify(m_option_map);
+
+  m_parsed_options = std::make_unique<po::parsed_options>(
+      po::command_line_parser(static_cast<int>(args.size()), args.data())
+          .options(m_group_vec.front().second)
+          .positional(pos)
+          .allow_unregistered()
+          .run());
+
+  po::store(*m_parsed_options, m_option_map);
 }
 
 int wallchanger::application::run() {
-  using namespace std::literals;
-  if (m_option_map.count("help") != 0U) {
-    fmt::print("\nusage: wallchanger [command] [option]\n\n");
-    ranges::for_each(m_group_vec[0].second.options(), [](auto data) {
-      fmt::print("{1:<20} {0:^15} {2:<20}\n"sv, "", data->format_name(),
+
+  if ((m_option_map.count("help") != 0U) &&
+      (m_option_map.count("command") == 0U)) {
+    fmt::print("\nusage: wallchanger [command] [argument]\n\n");
+    // subrange to exclude first 2 options
+    auto commandline_range =
+        ranges::make_subrange(m_group_vec[GLOBAL].second.options().begin() + 2,
+                              m_group_vec[GLOBAL].second.options().end());
+
+    ranges::for_each(commandline_range, [](auto data) {
+      fmt::print("{1:<20} {0:^15} {2:<20}\n", "", data->format_name(),
                  data->description());
     });
+    // subrange to exclude first subcommand
+    auto command_range =
+        ranges::make_subrange(m_group_name.begin() + 1, m_group_name.end());
+    fmt::print("\nAvaliable commands: {}", fmt::join(command_range, " , "));
   }
 
   if (m_option_map.count("version") != 0U) {
     fmt::print(stdout, "program version: {}\n", m_version);
   }
 
-  if (m_option_map.count("help-command") != 0U) {
-    const auto &cmd = m_option_map["help-command"].as<std::string>();
-    if (cmd == "history") {
-      fmt::print("\nusage: wallchanger [command] [option]\n\n");
-      ranges::for_each(m_group_vec[1].second.options(), [](auto data) {
-        fmt::print("{1:<20} {0:^15} {2:<20}\n"sv, "", data->format_name(),
-                   data->description());
-      });
-    } else if (cmd == "collection") {
-      fmt::print("\nusage: wallchanger [command] [option]\n\n");
-      ranges::for_each(m_group_vec[2].second.options(), [](auto data) {
-        fmt::print("{1:<20} {0:^15} {2:<20}\n"sv, "", data->format_name(),
-                   data->description());
-      });
+  if (m_option_map.count("command") != 0U) {
+    auto command = m_option_map["command"].as<std::string>();
+
+    if (command == "collection") {
+
+      auto options = po::collect_unrecognized(m_parsed_options->options,
+                                              po::include_positional);
+      options.erase(options.begin());
+      po::store(po::command_line_parser(options)
+                    .options(m_group_vec[COLLECTION].second)
+                    .run(),
+                m_option_map);
+
+      if (m_option_map.count("help") != 0U) {
+        ranges::for_each(m_group_vec[COLLECTION].second.options(),
+                         [](auto data) {
+                           fmt::print("{1:<20} {0:^15} {2:<20}\n", "",
+                                      data->format_name(), data->description());
+                         });
+      }
+
     } else {
-      fmt::print("{} is not a supported command\n", cmd);
+      fmt::print("{} command not supported\n", command);
     }
   }
-
   return 0;
 }
--- src/wall_app.h
+++ src/wall_app.h
@@ -7,13 +7,18 @@ namespace po = boost::program_options;
 
 class application {
   static constexpr std::string_view m_version = "0.1 pre alpha";
-  static constexpr int m_group_size = 6;
+  static constexpr int m_group_size = 4;
 
   using commandgroup = std::pair<std::string, po::options_description>;
   using commandline = std::vector<std::tuple<std::string_view, std::string_view,
                                              const po::value_semantic *>>;
+  enum subcommand_e { GLOBAL, COLLECTION, CONFIG, HISTORY };
+  enum command_loc { cmd, desc, value };
 
-  commandline m_general_options = {
+  commandline m_global_options = {
+      {"command", "command to execute", po::value<std::string>()},
+      {"subargs", "arguments to command",
+       po::value<std::vector<std::string>>()},
       {"next,n", "next wallpaper", nullptr},
       {"previous,p", "previous wallpaper", nullptr},
       {"get-current", "print informatin about current wallpaper", nullptr},
@@ -22,17 +27,26 @@ class application {
       {"version", "Program version", nullptr},
       {"help,h", "Print Help Message", nullptr}};
 
+  commandline m_collection_options{
+      {"create", "create new collection", po::value<std::string>()},
+      {"help,h", "print help message", nullptr}};
+
 public:
   explicit application(std::span<char *> args);
   int run();
 
 private:
+  // no default constructor for po::parsed_options hence unique_ptr
+  std::unique_ptr<po::parsed_options> m_parsed_options = nullptr;
   po::variables_map m_option_map;
+
   std::array<std::string, m_group_size> m_group_name = {
-      "general", "history", "collection", "configuration"};
+      "global", "collection", "configuration", "history"};
 
   std::vector<std::pair<std::string_view, commandline>> m_cmds = {
-      {m_group_name[0], m_general_options}};
+      {m_group_name[GLOBAL], m_global_options},
+      {m_group_name[COLLECTION], m_collection_options}};
+
   std::vector<commandgroup> m_group_vec;
 };
 } // namespace wallchanger
