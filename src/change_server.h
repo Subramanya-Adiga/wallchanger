2d96cccc278db5bd8455824d7744e07400512759
Subramanya <subramanya.adiga4@protonmail.com>
2023-03-08 22:18:28 +0530

    Expanded Response To Message Type
    Server Now Responds To Get Server Status MessageType.(Falling For Now)

--- src/change_server.h
+++ src/change_server.h
@@ -1,45 +1,71 @@
 #pragma once
 #include "message_type.h"
 #include "net/server_interface.h"
+#include <nlohmann/json.hpp>
 
 namespace wallchanger {
 class change_server : public net::server_interface<MessageType> {
 public:
   explicit change_server(uint16_t port)
-      : net::server_interface<MessageType>(port) {}
+      : net::server_interface<MessageType>(port) {
+    m_start_time = std::chrono::system_clock::now();
+  }
 
+protected:
   bool on_client_connect(
-      std::shared_ptr<net::connection<MessageType>> client) override {
+      std::shared_ptr<wallchanger::net::connection<wallchanger::MessageType>>
+          client) override {
+    wallchanger::net::message<wallchanger::MessageType> msg;
+    msg.header.id = wallchanger::MessageType::Client_Accepted;
+    msg << true;
+    client->send_message(msg);
     return true;
   }
 
   void on_client_disconnect(
       std::shared_ptr<net::connection<MessageType>> client) override {
-    LOG_INFO("server_logger", "Client:[{}] Removed", client->get_id());
+    LOG_INFO(get_logger_name(), "Client:[{}] Removed", client->get_id());
   }
 
-  void on_message(std::shared_ptr<net::connection<MessageType>> client,
-                  net::message<MessageType> &msg) override {
+  void on_message(
+      std::shared_ptr<wallchanger::net::connection<wallchanger::MessageType>>
+          client,
+      wallchanger::net::message<wallchanger::MessageType> &msg) override {
+
     switch (msg.header.id) {
-    case MessageType::Server_GetStatus:
-      break;
-    case MessageType::Server_GetPing: {
-      LOG_INFO("server_logger", "[{}] Server Ping\n", client->get_id());
+
+    case wallchanger::MessageType::Server_GetStatus: {
+      LOG_INFO(get_logger_name(), "[{}]:Requested Server Status\n",
+               client->get_id());
+      msg.body.clear();
+      nlohmann::json ret;
+      ret["connections"] = m_connections.size();
+      auto time_now = std::chrono::system_clock::now();
+      ret["uptime"] = std::chrono::time_point<std::chrono::system_clock>(
+                          time_now - m_start_time)
+                          .time_since_epoch()
+                          .count();
+      msg << ret;
       client->send_message(msg);
       break;
     }
-    case MessageType::Client_Accepted:
+
+    case wallchanger::MessageType::Server_GetPing: {
+      LOG_INFO(get_logger_name(), "[{}]: Server Ping\n", client->get_id());
+      msg << true;
+      client->send_message(msg);
       break;
-    case MessageType::Client_AssignID:
-      break;
-    case MessageType::Client_RegisterWithServer:
-      break;
-    case MessageType::Client_UnregisterWithServer:
+    }
+
+    default:
       break;
     }
   }
 
   void on_client_validated(
       std::shared_ptr<net::connection<MessageType>> client) override {}
+
+private:
+  std::chrono::time_point<std::chrono::system_clock> m_start_time;
 };
 } // namespace wallchanger
