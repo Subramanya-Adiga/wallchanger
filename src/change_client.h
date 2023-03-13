30ddc47c4cc539d02292876a09b9424b29b97927
Subramanya <subramanya.adiga4@protonmail.com>
2023-03-13 14:02:29 +0530

    Default Handling Of Messages. Added Default Handling For Added Messages.

--- src/change_client.h
+++ src/change_client.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "message_type.h"
 #include "net/client_interface.h"
-
+#include <fmt/chrono.h>
 namespace wallchanger {
 class change_client : public net::client_interface<MessageType> {
 public:
+  change_client() { m_active = connect("127.0.0.1", 60000); }
+
+  [[nodiscard]] bool active() const { return m_active; }
+
   void ping_server() {
     net::message<MessageType> msg;
     msg.header.id = MessageType::Server_GetPing;
@@ -19,11 +23,50 @@ public:
     send_message(msg);
   }
 
-  static nlohmann::json msg_to_json(const net::message<MessageType> &msg) {
-    std::vector<uint8_t> res;
-    res.resize(msg.body.size());
-    std::memcpy(res.data(), msg.body.data(), msg.body.size());
-    return nlohmann::json::from_cbor(res);
+  void run() {
+    while (!m_stop_processing) {
+      m_process_server_commands();
+    }
+  }
+
+private:
+  bool m_stop_processing = false;
+  bool m_active = false;
+
+  void m_process_server_commands() {
+    if (is_connected()) {
+      if (!incomming().empty()) {
+        auto msg = incomming().pop_front().msg;
+        if (msg.validate()) {
+          switch (msg.header.id) {
+          case MessageType::Server_GetStatus: {
+            std::cout << message_helper::msg_to_json(msg) << "\n";
+            m_stop_processing = true;
+          } break;
+          case MessageType::Server_GetPing: {
+            auto time_now = std::chrono::system_clock::now();
+            auto time_then = msg.time;
+            fmt::print("{}\n", std::chrono::duration_cast<std::chrono::seconds>(
+                                   time_now - time_then));
+            m_stop_processing = true;
+          } break;
+          case MessageType::Status_Success: {
+            m_stop_processing = true;
+          } break;
+          case MessageType::Client_Accepted:
+          case MessageType::Client_AssignID:
+          case MessageType::Client_RegisterWithServer:
+          case MessageType::Client_UnregisterWithServer:
+          case MessageType::Create_Collection:
+          case MessageType::Get_Next_Wallpaper:
+          case MessageType::Get_Previous_Wallpaper:
+          case MessageType::Change_Active_Collection:
+          case MessageType::Status_Failure:
+            break;
+          }
+        }
+      }
+    }
   }
 };
 } // namespace wallchanger
--- src/change_server.h
+++ src/change_server.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "message_type.h"
 #include "net/server_interface.h"
+#include "wall_cache.h"
 #include <nlohmann/json.hpp>
+
+#define MESSAGE_VALIDATE_BEGIN(x) if (x) {
+#define MESSAGE_VALIDATE_END                                                   \
+  }                                                                            \
+  else {                                                                       \
+    LOG_ERR(get_logger_name(), "Message Corrupted\n");                         \
+  }
 
 namespace wallchanger {
 
@@ -11,6 +19,8 @@ public:
       : net::server_interface<MessageType>(port) {
     m_start_time = std::chrono::system_clock::now();
   }
+
+  void store_state() { m_cache.serialize(); }
 
 protected:
   bool on_client_connect(
@@ -33,6 +43,9 @@ protected:
           client,
       wallchanger::net::message<wallchanger::MessageType> &msg) override {
 
+    MESSAGE_VALIDATE_BEGIN(msg.validate())
+
+    auto server_cmd = message_helper::msg_to_json(msg);
     switch (msg.header.id) {
 
     case wallchanger::MessageType::Server_GetStatus: {
@@ -45,20 +58,74 @@ protected:
                           time_now - m_start_time)
                           .time_since_epoch()
                           .count();
-      client->send_message(m_json_to_msg(MessageType::Server_GetStatus, ret));
-      break;
-    }
+      client->send_message(
+          message_helper::json_to_msg(MessageType::Server_GetStatus, ret));
+    } break;
 
     case wallchanger::MessageType::Server_GetPing: {
       LOG_INFO(get_logger_name(), "[{}]: Server Ping\n", client->get_id());
       msg << true;
       client->send_message(msg);
-      break;
-    }
+    } break;
 
-    default:
+    case wallchanger::MessageType::Change_Active_Collection: {
+      m_active.resize(msg.body.size());
+      std::memcpy(m_active.data(), msg.body.data(), msg.body.size());
+      LOG_INFO(get_logger_name(),
+               "Client:[{}] changed active collection to:{} \n",
+               client->get_id(), m_active);
+
+      client->send_message(m_success());
+    } break;
+
+    case wallchanger::MessageType::Create_Collection: {
+
+      wallchanger::cache_lib::cache_lib_type cache;
+      auto col_path = server_cmd["col_path"].get<std::string>();
+
+      int counter = 0;
+      auto inserter = [&](const std::filesystem::directory_entry &path) {
+        if (!path.is_directory()) {
+          counter++;
+          cache.insert(counter, path.path().filename().string());
+        }
+      };
+
+      if (!server_cmd["recursive"].get<bool>()) {
+        ranges::for_each(std::filesystem::directory_iterator(col_path),
+                         inserter);
+      } else {
+        ranges::for_each(
+            std::filesystem::recursive_directory_iterator(col_path), inserter);
+      }
+
+      m_cache.insert(server_cmd["new_col_name"].get<std::string>(), col_path,
+                     cache);
+      LOG_INFO(get_logger_name(), "created collection:[{}] path:[{}]\n",
+               server_cmd["new_col_name"].get<std::string>(),
+               server_cmd["col_path"].get<std::string>());
+
+      client->send_message(m_success());
+    } break;
+
+    case wallchanger::MessageType::Get_Next_Wallpaper: {
+      LOG_INFO(get_logger_name(), "Client:[{}] Requested Next Wallpaper\n",
+               client->get_id());
+    } break;
+
+    case wallchanger::MessageType::Get_Previous_Wallpaper: {
+      LOG_INFO(get_logger_name(), "Client:[{}] Requested Previous Wallpaper\n",
+               client->get_id());
+    } break;
+    case MessageType::Client_Accepted:
+    case MessageType::Client_AssignID:
+    case MessageType::Client_RegisterWithServer:
+    case MessageType::Client_UnregisterWithServer:
+    case MessageType::Status_Success:
+    case MessageType::Status_Failure:
       break;
     }
+    MESSAGE_VALIDATE_END
   }
 
   void on_client_validated(
@@ -66,16 +133,13 @@ protected:
 
 private:
   std::chrono::time_point<std::chrono::system_clock> m_start_time;
-
-  static net::message<MessageType> m_json_to_msg(MessageType type,
-                                                 nlohmann::json &json) {
-    wallchanger::net::message<wallchanger::MessageType> msg;
-    msg.header.id = type;
-    auto vec = nlohmann::json::to_cbor(json);
-    msg.body.resize(vec.size());
-    std::memcpy(msg.body.data(), vec.data(), vec.size());
-    msg.finalize();
-    return msg;
+  wallchanger::cache_lib m_cache;
+  std::string m_active;
+  inline static net::message<MessageType> m_success() {
+    net::message<MessageType> send;
+    send.header.id = MessageType::Status_Success;
+    send << true;
+    return send;
   }
 };
 } // namespace wallchanger
