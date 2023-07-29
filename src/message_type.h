e56766db5c97178895c2c5ffc0871b5a7a3f71a5
Subramanya <subramanya.adiga4@protonmail.com>
2023-07-29 22:44:42 +0530

    Server Error Codes.
    Added Server Error Code Enum And JSON Serialization Deserialization Helper.

--- src/message_type.h
+++ src/message_type.h
@@ -25,6 +25,17 @@ enum class MessageType : uint32_t {
   Status_Failure,
 };
 
+enum class server_error_code : uint32_t {
+  Message_Validation_Error,
+  Client_Connect_Error,
+  Client_Validation_Error
+};
+
+struct server_error {
+  server_error_code code;
+  std::string what;
+};
+
 struct message_helper {
   static nlohmann::json msg_to_json(const net::message<MessageType> &msg) {
     std::vector<uint8_t> res;
@@ -46,3 +57,28 @@ struct message_helper {
 };
 
 } // namespace wallchanger
+
+NLOHMANN_JSON_SERIALIZE_ENUM(
+    wallchanger::server_error_code,
+    {
+        {wallchanger::server_error_code::Client_Connect_Error,
+         "client_connect_error"},
+        {wallchanger::server_error_code::Client_Validation_Error,
+         "client_validation_error"},
+        {wallchanger::server_error_code::Message_Validation_Error,
+         "message_validation_error"},
+    });
+
+namespace nlohmann {
+template <> struct adl_serializer<wallchanger::server_error> {
+  static void to_json(json &j, const wallchanger::server_error &rhs) {
+    j = nlohmann::json{{"code", rhs.code}, {"error", rhs.what}};
+  }
+  static void from_json(const json &j, wallchanger::server_error &rhs) {
+    if (!j.is_null()) {
+      rhs.code = j.at("code").get<wallchanger::server_error_code>();
+      rhs.what = j.at("error").get<std::string>();
+    }
+  }
+};
+} // namespace nlohmann
