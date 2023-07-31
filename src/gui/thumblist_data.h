23463190b89bb503fc30b80eb80d8bee01f95f92
Subramanya <subramanya.adiga4@protonmail.com>
2023-07-31 23:12:21 +0530

    Updated ThumbList.
    Added Multithreading To Process Directories Faster, Made Class Emit Signals To Indicate New Image Added And For Signaling That Processing Directory Is Completed.

--- src/gui/thumblist_data.h
+++ src/gui/thumblist_data.h
@@ -1,18 +1,31 @@
 #pragma once
-
-#include <QHash>
 #include <QImage>
+#include <QObject>
+#include <QString>
+
+namespace wallchanger::gui {
+static constexpr int IMG_MEMORY_ALLOC = 1024;
+static constexpr int IMG_RES = 256;
 
 using thumb_cache_t = std::pair<QString, QImage>;
 using thumb_cache_list_t = QList<thumb_cache_t>;
 
-class ThumbListData {
+class ThumbListData : public QObject {
+  Q_OBJECT
 public:
-  ThumbListData() = default;
-  bool insert(QString name, QImage image);
-  thumb_cache_t get_data_at(int idx)const;
-  int count() const;
+  explicit ThumbListData(QObject *parent = nullptr);
+  void set_directory(const std::filesystem::path &path);
+  [[nodiscard]] bool insert(QString name, QImage image);
+  [[nodiscard]] thumb_cache_t get_data_at(int idx) const;
+  [[nodiscard]] int count() const;
+
+signals:
+  void thumb_nail_ready(QString, QImage);
+  void directory_processed();
+  void image_added(const QString &);
 
 private:
-  thumb_cache_list_t m_cache;
+  QScopedPointer<thumb_cache_list_t> m_cache;
 };
+
+} // namespace wallchanger::gui
