b4730c0ff18d6bd1a34ee66941e168fa48c02f34
Subramanya <subramanya.adiga4@protonmail.com>
2023-08-02 02:59:15 +0530

    Updated ThumbListModel
    Model Now Adds Data Into ThumblistData. Inserting Data As Entire Directory Only Supported.

--- src/gui/thumblist_model.cpp
+++ src/gui/thumblist_model.cpp
@@ -1,31 +1,102 @@
 #include "thumblist_model.h"
+#include <QFutureWatcher>
+#include <QImageReader>
+#include <QPixmap>
+#include <QtConcurrent>
 
-ThumbListModel::ThumbListModel(QObject *parent) : QAbstractListModel{parent} {}
+wallchanger::gui::ThumbListModel::ThumbListModel(const QString &path,
+                                                 QObject *parent)
+    : QAbstractListModel(parent),
+      m_data(new wallchanger::gui::ThumbListData()) {
 
-int ThumbListModel::rowCount(const QModelIndex &parent) const {
-  if (!parent.isValid()) {
-    return 0;
-  }
-  return m_data.count();
+  connect(this, &wallchanger::gui::ThumbListModel::thumb_nail_ready, this,
+          &wallchanger::gui::ThumbListModel::m_add_data);
+
+  m_set_directory(path.toStdString());
 }
 
-QVariant ThumbListModel::data(const QModelIndex &index, int role) const {
+int wallchanger::gui::ThumbListModel::rowCount(
+    const QModelIndex &parent) const {
+  return parent.isValid() ? 0 : m_data->count();
+}
+
+QVariant wallchanger::gui::ThumbListModel::data(const QModelIndex &index,
+                                                int role) const {
   if (!index.isValid()) {
     return {};
   }
 
-  const auto &data = m_data.get_data_at(index.row());
+  const auto &data = m_data->get_data_at(index.row());
+
   if (role == Qt::DisplayRole) {
     return data.first;
   }
+
   if (role == Qt::DecorationRole) {
-    return data.second;
+    return QPixmap::fromImage(data.second)
+        .scaled(128, 128, Qt::KeepAspectRatio, Qt::SmoothTransformation);
   }
 
-  return {};
+  if (role != Qt::DisplayRole && role != Qt::EditRole) {
+    return {};
+  }
+
+  return m_data->get_data_at(index.row()).first;
 }
 
-void ThumbListModel::set_model_data(ThumbListData new_data)
-{
-  m_data = new_data;
+const wallchanger::gui::ThumbListData *
+wallchanger::gui::ThumbListModel::get_model_data() const {
+  return m_data.get();
+}
+
+void wallchanger::gui::ThumbListModel::m_add_data(QString name, QImage image) {
+  beginInsertRows(QModelIndex(), m_counter, m_counter);
+  (void)m_data->insert(std::move(name), std::move(image));
+  endInsertRows();
+  m_counter++;
+}
+
+void wallchanger::gui::ThumbListModel::m_set_directory(
+    const std::filesystem::path &path) {
+  if (std::filesystem::exists(path) && std::filesystem::is_directory(path)) {
+
+    bool generate{true};
+    auto *watcher = new QFutureWatcher<void>();
+
+    connect(QCoreApplication::instance(), &QCoreApplication::aboutToQuit,
+            [&]() { generate = false; });
+
+    connect(watcher, &QFutureWatcher<void>::finished, [this, watcher] {
+      watcher->deleteLater();
+      emit directory_processed();
+    });
+
+    auto filesystem_range = ranges::make_subrange(
+        std::filesystem::begin(std::filesystem::directory_iterator(path)),
+        std::filesystem::end(std::filesystem::directory_iterator()));
+
+    auto fs_vec = filesystem_range |
+                  ranges::views::remove_if(
+                      [](const auto &entry) { return entry.is_directory(); }) |
+                  ranges::views::transform(
+                      [](const auto &entry) { return entry.path(); }) |
+                  ranges::to<QList>();
+
+    QFuture<void> res;
+    for (auto &&entries : fs_vec) {
+      if (!generate) {
+        break;
+      }
+      res = QtConcurrent::run(
+          [this](const auto &entry) {
+            QImage image(entry.string().c_str());
+            QImageReader::setAllocationLimit(IMG_MEMORY_ALLOC);
+            auto scaled = image.scaled({IMG_RES, IMG_RES}, Qt::KeepAspectRatio);
+            scaled.convertTo(QImage::Format_ARGB32);
+            emit thumb_nail_ready(entry.filename().string().c_str(), scaled);
+          },
+          entries);
+    }
+    watcher->setFuture(res);
+  }
 }
--- src/gui/thumblist_model.h
+++ src/gui/thumblist_model.h
@@ -1,16 +1,31 @@
 #pragma once
 
-#include <QAbstractListModel>
 #include "thumblist_data.h"
+#include <QAbstractListModel>
+
+namespace wallchanger::gui {
+
+static constexpr int IMG_MEMORY_ALLOC = 1024;
+static constexpr int IMG_RES = 256;
 
 class ThumbListModel : public QAbstractListModel {
+  Q_OBJECT
 public:
-  explicit ThumbListModel(QObject *parent = nullptr);
+  explicit ThumbListModel(const QString &path, QObject *parent = nullptr);
 
   [[nodiscard]] int rowCount(const QModelIndex &parent) const override;
-  [[nodiscard]] QVariant data(const QModelIndex &index, int role) const override;
+  [[nodiscard]] QVariant data(const QModelIndex &index,
+                              int role) const override;
+  [[nodiscard]] const ThumbListData *get_model_data() const;
 
-  void set_model_data(ThumbListData new_data);
+signals:
+  void thumb_nail_ready(QString, QImage);
+  void directory_processed();
+
 private:
-  ThumbListData m_data;
+  QSharedPointer<ThumbListData> m_data;
+  int m_counter{0};
+  void m_add_data(QString name, QImage image);
+  void m_set_directory(const std::filesystem::path &path);
 };
+} // namespace wallchanger::gui
