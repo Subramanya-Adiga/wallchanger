04a6ea52e2e8d89dad13333ebec080f67a2393b0
Subramanya <subramanya.adiga4@protonmail.com>
2024-01-11 14:26:23 +0530

    Added Debug Server Start
    Server Starts In Standalone Mode While Debugging.

--- src/service_main.cpp
+++ src/service_main.cpp
@@ -1,7 +1,20 @@
 #include "change_service.h"
 
+#ifdef _DEBUG
+wallchanger::change_service service_debug;
+bool WINAPI close_handle(DWORD /*sig*/) {
+  service_debug.stop(0);
+  return true;
+}
+#endif
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+#ifndef _DEBUG
   wallchanger::change_service service;
   wallchanger::platform::win32::service_base::run(service);
+#else
+  SetConsoleCtrlHandler((PHANDLER_ROUTINE)close_handle, 1);
+  service_debug.start(0);
+#endif
   return 0;
 }
