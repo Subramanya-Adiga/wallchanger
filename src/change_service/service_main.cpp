cbd8d838dd308b87c910865a54ae317c2e21afbc
Subramanya <subramanya.adiga4@protonmail.com>
2024-03-01 21:26:52 +0530

    Running On Linx.

--- src/change_service/service_main.cpp
+++ src/change_service/service_main.cpp
@@ -22,5 +22,33 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
 #endif
 
 #ifdef __linux__
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) { return 0; }
+#include "change_server.h"
+namespace {
+bool stop_server = false;
+}
+
+void app_exit([[maybe_unused]] int sig) { stop_server = true; }
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
+  wallchanger::change_server server(wallchanger::helper::s_port_number);
+
+  if (!std::filesystem::exists(data_directory())) {
+    std::filesystem::create_directories(data_directory());
+  }
+
+  if (!std::filesystem::exists(log_directory())) {
+    std::filesystem::create_directories(log_directory());
+  }
+
+  bool running = server.start();
+  (void)signal(SIGINT, app_exit);
+  if (running) {
+    while (!stop_server) {
+      server.update(-1, false);
+    }
+    server.store_state();
+  }
+
+  return 0;
+}
 #endif
--- src/config.h
+++ src/config.h
@@ -12,11 +12,19 @@ inline std::string log_directory() {
   std::string ret = std::getenv("userprofile");
   return ret + R"(\AppData\Local\Wallchanger\logs\)";
 #endif
+#ifdef __linux__
+  std::string ret = std::getenv("HOME");
+  return ret + R"(/.local/share/wallchanger/logs/)";
+#endif
 }
 
 inline std::string data_directory() {
 #ifdef _WINDOWS
   std::string ret = std::getenv("userprofile");
   return ret + R"(\AppData\Local\Wallchanger\data\)";
+#endif
+#ifdef __linux__
+  std::string ret = std::getenv("HOME");
+  return ret + R"(/.config/wallchanger/data/)";
 #endif
 }
