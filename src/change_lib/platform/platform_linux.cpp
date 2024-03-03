d90dd72afa0ea7a17d2efb6cc86c364483a81dcc
Subramanya <subramanya.adiga4@protonmail.com>
2024-03-03 15:39:23 +0530

    Implemented Lua Script Execution.

--- src/change_lib/platform/platform_linux.cpp
+++ src/change_lib/platform/platform_linux.cpp
@@ -2,15 +2,23 @@
 
 namespace wallchanger::platform {
 
+linux::linux_background_handler::linux_background_handler() {
+  m_lua_state.open_libraries(sol::lib::base, sol::lib::os);
+}
 bool linux::linux_background_handler::is_active() { return true; }
 
 bool linux::linux_background_handler::set_background_color(
     unsigned long color) {
+  m_lua_state["bg_color"] = color;
   return true;
 }
 
 bool linux::linux_background_handler::set_wallpaper(std::string_view wallpaper,
                                                     int position) {
+  m_lua_state["background"] = wallpaper;
+  m_lua_state["position"] = position;
+  auto res =
+      m_lua_state.load_file(data_directory() + "/plugins/linux_changer.lua");
   return true;
 }
 
--- src/change_lib/platform/platform_linux.h
+++ src/change_lib/platform/platform_linux.h
@@ -1,17 +1,20 @@
 #pragma once
 #include "../wall_background.h"
-
+#include <sol/sol.hpp>
 namespace wallchanger::platform::linux {
 
 class linux_background_handler : public wallchanger::background_handler {
 public:
-  linux_background_handler() = default;
+  linux_background_handler();
   ~linux_background_handler() override = default;
   [[nodiscard]] bool is_active() override;
   [[nodiscard]] bool set_wallpaper(std::string_view wallpaper,
                                    int position) override;
   [[nodiscard]] bool set_background_color(unsigned long color) override;
   [[nodiscard]] std::string get_error() const;
+
+private:
+  sol::state m_lua_state;
 };
 
 } // namespace wallchanger::platform::linux
